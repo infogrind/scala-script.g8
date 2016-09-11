5891ea6969eb5f64cfe4b946597ed04d3509c7d5
Marius Kleiner <kleiner@gmail.com>
2016-09-11 09:06:46 +0200

    Run script for instantiated template
    This makes it possible to automatically instanciate and run the script template
    and should help with testing.
    
    Currently there is still some problem with parameter passing - if the run.sh
    script has more than one argument, sbt only recognizes the first one of those as
    a parameter to the csRun command. -> To be fixed

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TARGETDIR=target
+TESTDIR=$TARGETDIR/giter8
+G8DIR=`pwd`
+SCRIPTNAME=testscript
+
+mkdir -p "$TARGETDIR" && rm -Rf "$TESTDIR" && mkdir -p "$TESTDIR"
+
+cd "$TESTDIR"
+
+g8 "file://$G8DIR" \
+    --name=$SCRIPTNAME \
+    --organization=ch.hostuff.scalascript \
+    --version=0.0.1 \
+    --scalaversion=2.11.5
+
+cd "$SCRIPTNAME"
+
+sbt "csRun $SCRIPTNAME $@"
+
+cd "$G8DIR"
