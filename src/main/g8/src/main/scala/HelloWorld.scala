f990cb337d40c1ec9e1103bebafc6b020adcef5a
Marius Kleiner <kleiner@gmail.com>
2016-09-11 09:15:57 +0200

    Combined conscript and regular entry point
    The main class now has a scala-conform main method. If it is called via the
    conscript entry point, the main method is indirectly called.
    (Copied from how it is done in the conscript source code.)

--- src/main/g8/src/main/scala/HelloWorld.scala
+++ src/main/g8/src/main/scala/HelloWorld.scala
@@ -1,14 +1,22 @@
 package $organization$.$name;format="lower,word"$ 
 
+object HelloWorld {
+  def main(args: Array[String]) {
+    val exit = run(args);
+  }
+
+  def run(args: Array[String]) : Int = {
+    // Print a message and the arguments to the application
+    println("Hello world!")
+    args.foreach(println)
+    0
+  }
+}
+
+
 class HelloWorld extends xsbti.AppMain {
   def run(configuration: xsbti.AppConfiguration): xsbti.MainResult = {
-    val scalaVersion = configuration.provider.scalaProvider.version
-
-    // Print a message and the arguments to the application
-    println("Hello world!  Running Scala " + scalaVersion)
-    configuration.arguments.foreach(println)
-
-    new Exit(0)
+    new Exit(HelloWorld.run(configuration.arguments))
   }
   class Exit(val code: Int) extends xsbti.Exit
 }
